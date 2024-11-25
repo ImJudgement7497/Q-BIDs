6891b8b014fe0c0a3e1c731b707017c13e7a0d1f
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-11-25 00:52:50 +0000

    Created Debugging class and function to read the potential from an image

--- solver.py
+++ solver.py
@@ -26,3 +26,48 @@
 #                 H[j, j + int(np.sqrt(N))] = -1 / denom
 
 #     return H
+
+# PLAN
+# Construct Sparse Matrix either using function above or similar way
+# Solve eigenvalues/vectors and check against inspo
+# Plot
+# Optimise
+# Parallise
+
+# Optional Features: adding the boundary back on
+
+import argparse
+import os
+import pickle
+import numpy as np
+from PIL import Image
+from Debugging import Debugging
+
+# Function to get potential from image
+def get_potential_from_image(file_name: str) -> tuple: 
+
+    # Open image in black and white
+    image = (Image.open(file_name)).convert('1')
+
+    # Create an image matrix with white pixels being false, and black being true
+    bool_image_matrix = np.array(image) == 0
+
+    # Get number of points in y and x (num of rows/cols in the original image)
+    total_y_points, total_x_points = bool_image_matrix.shape
+
+    # Create a mesh for when constructing the Hamiltonian
+    position_mesh_matrix = np.where(bool_image_matrix, 0, 1)
+
+    # Save debug information
+    debugger.debug_store(bool_image_matrix, "./debug/bool_image.mat")
+    debugger.debug_store(position_mesh_matrix, "./debug/position_mesh.mat")
+    
+    return (position_mesh_matrix.flatten(), (total_x_points, total_y_points))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+args = parser.parse_args()
+
+debugger = Debugging(debug=args.debug)
+
+temp = get_potential_from_image(file_name="./three.png")
